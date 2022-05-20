528eeeb82149a197d5e6200e005e69afccd3d7d4
Samuel Monroe <spat.monroe@gmail.com>
2022-05-20 08:34:19 +0200

    Take a number - Processes

---
+++ elixir/take-a-number/lib/take_a_number.ex
@@ -0,0 +1,22 @@
+defmodule TakeANumber do
+  def start() do
+    spawn(__MODULE__, :counter, [0])
+  end
+
+  def counter(number) do
+    receive do
+      {:take_a_number, sender} ->
+        new_number = number+1
+        send(sender, new_number)
+        counter(new_number)
+      {:report_state, sender} ->
+        send(sender, number)
+        counter(number)
+      :stop ->
+        :ok
+      _ ->
+        counter(number)
+    end
+
+  end
+end
