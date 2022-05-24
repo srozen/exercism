bd2455c6fc6c91e4fd8a9db3371352f5d16c5b8d
Samuel Monroe <spat.monroe@gmail.com>
2022-05-24 10:14:36 +0200

    Enums

--- elixir/boutique-inventory/lib/boutique_inventory.ex
+++ elixir/boutique-inventory/lib/boutique_inventory.ex
@@ -1,21 +1,33 @@
 defmodule BoutiqueInventory do
   def sort_by_price(inventory) do
-    # Please implement the sort_by_price/1 function
+    inventory
+    |> Enum.sort_by(&(&1.price), :asc)
   end
 
   def with_missing_price(inventory) do
-    # Please implement the with_missing_price/1 function
+    inventory
+    |> Enum.filter(&(&1.price == nil))
   end
 
   def update_names(inventory, old_word, new_word) do
-    # Please implement the update_names/3 function
+    inventory
+    |> Enum.map(&(update_item_name(&1, old_word, new_word)))
+  end
+
+  defp update_item_name(item, old_word, new_word) do
+    item
+    |> Map.update!(:name, fn name -> String.replace(name, old_word, new_word) end)
   end
 
   def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
+    updated_quantities = item.quantity_by_size
+    |> Enum.into(Map.new, fn {key, val} -> {key, val + count} end)
+
+    Map.put(item, :quantity_by_size, updated_quantities)
   end
 
   def total_quantity(item) do
-    # Please implement the total_quantity/1 function
+    item.quantity_by_size
+    |> Enum.reduce(0, fn {_key, val}, acc -> val + acc end)
   end
 end
