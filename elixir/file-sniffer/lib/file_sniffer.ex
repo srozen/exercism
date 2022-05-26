42abb568fa19f3846bf6acba783f963e2c774662
Samuel Monroe <spat.monroe@gmail.com>
2022-05-26 22:34:27 +0200

    File sniffer and binaries

--- elixir/file-sniffer/lib/file_sniffer.ex
+++ elixir/file-sniffer/lib/file_sniffer.ex
@@ -1,13 +1,28 @@
 defmodule FileSniffer do
   def type_from_extension(extension) do
-    # Please implement the type_from_extension/1 function
+    case extension do
+      "exe" -> "application/octet-stream"
+      "bmp" -> "image/bmp"
+      "png" -> "image/png"
+      "jpg" -> "image/jpg"
+      "gif" -> "image/gif"
+    end
   end
 
-  def type_from_binary(file_binary) do
-    # Please implement the type_from_binary/1 function
-  end
+  def type_from_binary(<<0x7f, 0x45, 0x4c, 0x46, _rest::binary>>), do: "application/octet-stream"
+  def type_from_binary(<<0x42, 0x4D, _rest::binary>>), do: "image/bmp"
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _rest::binary>>), do: "image/png"
+  def type_from_binary(<<0xFF, 0xD8, 0xFF, _rest::binary>>), do: "image/jpg"
+  def type_from_binary(<<0x47, 0x49, 0x46, _rest::binary>>), do: "image/gif"
+  def type_from_binary(_), do: :error
 
   def verify(file_binary, extension) do
-    # Please implement the verify/2 function
+    type_to_verify = type_from_extension(extension)
+    case type_from_binary(file_binary) do
+      type when type == type_to_verify ->
+        {:ok, type_to_verify}
+      _ ->
+        {:error, "Warning, file format and file extension do not match."}
+    end
   end
 end
