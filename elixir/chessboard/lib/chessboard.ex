4d93e112748316bc30a56f137636b95755b4a005
Samuel Monroe <spat.monroe@gmail.com>
2022-07-29 21:26:46 +0200

    Chessboard solution

--- elixir/chessboard/lib/chessboard.ex
+++ elixir/chessboard/lib/chessboard.ex
@@ -1,17 +1,19 @@
 defmodule Chessboard do
   def rank_range do
-    # Please implement the rank_range/0 function
+    1..8
   end
 
   def file_range do
-    # Please implement the file_range/0 function
+    ?A..?H
   end
 
   def ranks do
-    # Please implement the ranks/0 function
+    rank_range()
+    |> Enum.map(&(&1))
   end
 
   def files do
-    # Please implement the files/0 function
+    file_range()
+    |> Enum.map(&(List.to_string([&1])))
   end
 end
