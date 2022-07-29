f8e3d54745c9ce1e9b153fb20ab001e51f388219
Samuel Monroe <spat.monroe@gmail.com>
2022-07-29 21:07:59 +0200

    Newsletter : File manipulations

--- elixir/newsletter/lib/newsletter.ex
+++ elixir/newsletter/lib/newsletter.ex
@@ -1,21 +1,33 @@
 defmodule Newsletter do
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    File.read!(path)
+    |> String.split("\n")
+    |> Enum.filter(fn e -> e != "" end)
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    File.open!(path, [:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    IO.puts(pid, email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    File.close(pid)
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    file = open_log(log_path)
+
+    read_emails(emails_path)
+    |> Enum.each(fn email ->
+      case send_fun.(email) do
+        :ok -> log_sent_email(file, email)
+        _ -> :ok
+      end
+    end)
+
+    close_log(file)
   end
 end
