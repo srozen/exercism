e324856a592bc15dabdb996b2285ce5dcdbe360f
Samuel Monroe <spat.monroe@gmail.com>
2023-01-17 14:19:27 +0100

    High scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,30 @@
+=begin
+Write your code for the 'High Scores' exercise in this file. Make the tests in
+`high_scores_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/high-scores` directory.
+=end
+
+class HighScores
+  attr_accessor :scores
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores[-1]
+  end
+
+  def personal_best
+    @scores.max(1)[0]
+  end
+
+  def personal_top_three
+    @scores.max(3)
+  end
+
+  def latest_is_personal_best?
+    @scores.last == personal_best
+  end
+end
