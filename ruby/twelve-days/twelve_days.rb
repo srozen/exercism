046fa65ac83349634162e76d7a36416c93afa23f
Samuel Monroe <spat.monroe@gmail.com>
2023-01-26 06:24:22 +0100

    Second iteration of Twelve days, cleaner

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -8,30 +8,25 @@ To get started with TDD, see the `README.md` file in your
 module TwelveDays
   class << self
     PRESENTS_PER_DAY = {
-      "first" => "a Partridge",
-      "second" => "two Turtle Doves",
-      "third" => "three French Hens",
-      "fourth" => "four Calling Birds",
-      "fifth" => "five Gold Rings",
-      "sixth" => "six Geese-a-Laying",
-      "seventh" => "seven Swans-a-Swimming",
-      "eighth" => "eight Maids-a-Milking",
-      "ninth" => "nine Ladies Dancing",
-      "tenth" => "ten Lords-a-Leaping",
-      "eleventh" => "eleven Pipers Piping",
-      "twelfth" => "twelve Drummers Drumming"
+      "first" => ", and a Partridge",
+      "second" => ", two Turtle Doves",
+      "third" => ", three French Hens",
+      "fourth" => ", four Calling Birds",
+      "fifth" => ", five Gold Rings",
+      "sixth" => ", six Geese-a-Laying",
+      "seventh" => ", seven Swans-a-Swimming",
+      "eighth" => ", eight Maids-a-Milking",
+      "ninth" => ", nine Ladies Dancing",
+      "tenth" => ", ten Lords-a-Leaping",
+      "eleventh" => ", eleven Pipers Piping",
+      "twelfth" => ", twelve Drummers Drumming"
     }.freeze
 
     def song
-      PRESENTS_PER_DAY.each_with_object({lyrics: "", past_presents: ""}) do |(day, present), saved_items|
-        first_day = day == "first"
-        last_day = day == "twelfth"
-
-        separator = first_day ? ", and " : ", "
-        line_returns = last_day ? "\n" : "\n\n"
-        saved_items[:lyrics] << "On the #{day} day of Christmas my true love gave to me: #{present}#{saved_items[:past_presents]} in a Pear Tree.#{line_returns}"
-
-        saved_items[:past_presents] = separator << present << saved_items[:past_presents]
+      PRESENTS_PER_DAY.each_with_object({lyrics: "", past_presents: ""}) do |(day, present), result|
+        result[:lyrics] << "On the #{day} day of Christmas my true love gave to me: #{present.gsub(/,( and)? /, "")}#{result[:past_presents]} in a Pear Tree.\n"
+        result[:lyrics] << "\n" unless day == "twelfth"
+        result[:past_presents] = present << result[:past_presents]
       end[:lyrics]
     end
   end
