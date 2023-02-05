ce0f5f6dc4ede6a971a3d9eb84760c24e7f6ad3a
Samuel Monroe <spat.monroe@gmail.com>
2023-02-05 22:06:38 +0100

    Cleanup

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,23 +1,18 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
 class SpaceAge
   attr_reader :earth_age
 
-  EARTH_YEAR_IN_SECONDS = 31_557_600.0.freeze
+  EARTH_YEAR_IN_SECONDS = 31_557_600.0
 
   RELATIVE_AGE_PER_PLANET = {
-    "mercury" => 0.2408467,
-    "venus" => 0.61519726,
-    "mars" => 1.8808158,
-    "jupiter" => 11.862615,
-    "saturn" => 29.447498,
-    "uranus" => 84.016846,
-    "neptune" => 164.79132,
+    'mercury' => 0.2408467,
+    'venus' => 0.61519726,
+    'mars' => 1.8808158,
+    'jupiter' => 11.862615,
+    'saturn' => 29.447498,
+    'uranus' => 84.016846,
+    'neptune' => 164.79132
   }.freeze
 
   def initialize(age_in_seconds)
@@ -29,7 +24,7 @@ class SpaceAge
   end
 
   RELATIVE_AGE_PER_PLANET.each do |planet, orbital_period|
-    define_method :"on_#{planet}" do
+    define_method "on_#{planet}" do
       earth_age / orbital_period
     end
   end
