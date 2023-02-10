797ea9a7b97fe8991f1e3e3dc3063fc7c1f1ef37
Samuel Monroe <spat.monroe@gmail.com>
2023-02-10 18:31:51 +0100

    Ruby difference of squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,19 @@
+
+class Squares
+  attr_reader :range
+  def initialize(n)
+    @range = 1..n
+  end
+
+  def square_of_sum
+    @range.inject(&:+)**2
+  end
+
+  def sum_of_squares
+    @range.map { |n| n**2 }.inject(&:+)
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+end
