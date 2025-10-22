ae10f76d88c5f60a898fc1f7d34ab058fac95392
Samuel Monroe <spat.monroe@gmail.com>
2025-10-22 13:40:13 +0900

    Ruby - Two Fer

---
+++ ruby/two-fer/two_fer_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require_relative 'two_fer'
+
+class TwoFerTest < Minitest::Test
+  def test_no_name_given
+    actual = TwoFer.two_fer
+    expected = 'One for you, one for me.'
+    assert_equal expected, actual
+  end
+
+  def test_a_name_given
+    actual = TwoFer.two_fer('Alice')
+    expected = 'One for Alice, one for me.'
+    assert_equal expected, actual
+  end
+
+  def test_another_name_given
+    actual = TwoFer.two_fer('Bob')
+    expected = 'One for Bob, one for me.'
+    assert_equal expected, actual
+  end
+end
