0dc2a4456a04b97e53eb4ef0ff1617be89909361
Samuel Monroe <spat.monroe@gmail.com>
2025-10-24 10:17:55 +0200

    Ruby - Eliud's Eggs - Better Solution

--- ruby/eliuds-eggs/eliuds_eggs.rb
+++ ruby/eliuds-eggs/eliuds_eggs.rb
@@ -1,12 +1,8 @@
 module EliudsEggs
-  def self.egg_count(n)
-    EliudsEggs.egg_count_helper(n)
-  end
-
-  def self.egg_count_helper(n, accumulator = 0)
+  def self.egg_count(n, accumulator = 0)
     return accumulator if n <= 0
 
     accumulator += 1 if n % 2 == 1
-    egg_count_helper(n / 2, accumulator)
+    EliudsEggs.egg_count(n / 2, accumulator)
   end
 end
